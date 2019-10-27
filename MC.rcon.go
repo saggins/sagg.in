c1bb87aecea75677c1a2f0d420e9a9c773c27c41
win32prog <win32prog@gmail.com>
2019-10-27 11:22:54 -0700

    safety

--- MC.rcon.go
+++ MC.rcon.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"io/ioutil"
+
 	rcon "github.com/Tnze/go-mc/net"
 )
 
 func RconWhitelist(whitelist []Whitelist) {
-	l, err := rcon.DialRCON("0.0.0.0:25575", "1234")
+	pass, err := ioutil.ReadFile("password.txt")
+	l, err := rcon.DialRCON("172.31.18.164:25575", string(pass))
 
 	if err != nil {
 		panic(err.Error())
